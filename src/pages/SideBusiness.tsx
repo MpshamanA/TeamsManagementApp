34bff497c4c7d35774da6e47cbeb1715bbf98c4d
shoIwasaki <m.pshaman.a@gmail.com>
2022-03-22 22:40:00 +0900

    図書制度、副業制度文言追加

--- src/pages/SideBusiness.tsx
+++ src/pages/SideBusiness.tsx
@@ -19,7 +19,18 @@ const SideBusiness: React.FC<RouteComponentProps> = (props) => {
         <div className={style.header}>
           <Header history={props.history} />
         </div>
-        <div className={style.mainBenefits}>副業コンテンツ</div>
+        <div className={style.mainBenefits}>
+          副業コンテンツ ◆副業許可申請詳細◆
+          ---------------------------------------------------------------------------------
+          ■対象者 ・当社社員全員 ■申請方法
+          ・添付されている資料をダウンロードしていただき
+          記載後に岩本までDMにて送付してください。 ■提出期日
+          ・副業が決まり次第随時 ■注意事項
+          ・副業される方は年末調整の関係も有るため、必ず申請をお願い致します
+          ・副業される方は税務署へ開業届の提出をしましょう
+          ・副業される方は必ず確定申告をしましょう
+          ・確定申告や開業申請など不明点あればお問合せください
+        </div>
       </div>
     </>
   );
