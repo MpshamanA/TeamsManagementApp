74ddf392daf5b66aed29df7edb0280c0431024f7
shoIwasaki <m.pshaman.a@gmail.com>
2022-03-24 23:22:05 +0900

    副業、図書制度ページのUI修正

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -5,6 +5,8 @@ import { Header } from "../components/Header";
 import { Side } from "../components/Side";
 import { manuContext } from "../Store";
 
+import Typography from "@mui/material/Typography";
+
 import style from "../css/common.module.scss";
 
 const Library: React.FC<RouteComponentProps> = (props) => {
@@ -19,19 +21,47 @@ const Library: React.FC<RouteComponentProps> = (props) => {
           <Header history={props.history} />
         </div>
         <div className={style.mainBenefits}>
-          図書コンテンツ ◆図書購入_貸出制度◆
-          -----------------------------------------------------------------------------
-          ■対象者 ・全社員対象 ■毎月の上限金額 ・毎月一人5千円まで
-          ※当社在籍中であれば何回でも購入可 ■購入申請できる本
-          ・技術本、自己啓発などスキルアップに伴うものであれば何でもOK。
-          ■購入方法 ・添付のフォーマットから申請をお願い致します。
-          ※DMで岩本まで送付お願いします ■購入後のフロー
-          ・購入後に貸し出しという形になるので、
-          直接ご自宅へ配送又は別途お会いした時にお渡し致します。
-          ・読み終わったあとは、上記同様にお会いした際や郵送で返却をお願いします。
-          (本の保有元は会社となりますのでご注意下さい) ■現在貸出可能な書籍
-          ・一覧表を作成しました。 ご希望の方購入申請書別タブの
-          書籍貸出申請書から申請をお願いします。
+          <Typography gutterBottom variant="h5" component="div" sx={{}}>
+            図書購入_貸出制度
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{}}>
+            対象者
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{}}>
+            全社員対象
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{}}>
+            毎月の上限金額
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{}}>
+            毎月一人5千円まで
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{}}>
+            購入申請できる本
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{}}>
+            技術本、自己啓発などスキルアップに伴うものであれば何でもOK。
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{}}>
+            添付のフォーマットから申請をお願い致します。※DMで岩本まで送付お願いします
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{}}>
+            購入後のフロー
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{}}>
+            購入後に貸し出しという形になるので、
+            直接ご自宅へ配送又は別途お会いした時にお渡し致します。
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{}}>
+            読み終わったあとは、上記同様にお会いした際や郵送で返却をお願いします。(本の保有元は会社となりますのでご注意下さい)
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{}}>
+            現在貸出可能な書籍
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{}}>
+            一覧表を作成しました。 ご希望の方購入申請書別タブの
+            書籍貸出申請書から申請をお願いします。
+          </Typography>
         </div>
       </div>
     </>
--- src/pages/SideBusiness.tsx
+++ src/pages/SideBusiness.tsx
@@ -6,6 +6,8 @@ import { Header } from "../components/Header";
 import { Side } from "../components/Side";
 import { manuContext } from "../Store";
 
+import Typography from "@mui/material/Typography";
+
 import style from "../css/common.module.scss";
 
 const SideBusiness: React.FC<RouteComponentProps> = (props) => {
@@ -20,16 +22,34 @@ const SideBusiness: React.FC<RouteComponentProps> = (props) => {
           <Header history={props.history} />
         </div>
         <div className={style.mainBenefits}>
-          副業コンテンツ ◆副業許可申請詳細◆
-          ---------------------------------------------------------------------------------
-          ■対象者 ・当社社員全員 ■申請方法
-          ・添付されている資料をダウンロードしていただき
-          記載後に岩本までDMにて送付してください。 ■提出期日
-          ・副業が決まり次第随時 ■注意事項
-          ・副業される方は年末調整の関係も有るため、必ず申請をお願い致します
-          ・副業される方は税務署へ開業届の提出をしましょう
-          ・副業される方は必ず確定申告をしましょう
-          ・確定申告や開業申請など不明点あればお問合せください
+          <Typography gutterBottom variant="h5" component="div" sx={{}}>
+            ◆副業許可申請詳細◆
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{}}>
+            対象者
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{}}>
+            当社社員全員
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{}}>
+            添付されている資料をダウンロードしていただき
+            記載後に岩本までDMにて送付してください。
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{}}>
+            提出期日
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{}}>
+            副業が決まり次第随時
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{}}>
+            注意事項
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{}}>
+            副業される方は年末調整の関係も有るため、必ず申請をお願い致します
+            副業される方は税務署へ開業届の提出をしましょう
+            副業される方は必ず確定申告をしましょう
+            確定申告や開業申請など不明点あればお問合せください
+          </Typography>
         </div>
       </div>
     </>
